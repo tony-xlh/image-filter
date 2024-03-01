1ef2bddb884bb2691b4305129d8509411d26ea00
xulihang <xulihanghai@163.com>
2024-03-01 16:07:36 +0800

    add InvertFilter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { BlackwhiteFilter } from "./BlackwhiteFilter";
 import {GrayscaleFilter} from "./GrayscaleFilter";
+import { InvertFilter } from "./InvertFilter";
 import { SepiaFilter } from "./SepiaFilter";
 
 console.log(GrayscaleFilter);
 (window as any)["GrayscaleFilter"] = GrayscaleFilter;
 (window as any)["SepiaFilter"] = SepiaFilter;
+(window as any)["InvertFilter"] = InvertFilter;
 (window as any)["BlackwhiteFilter"] = BlackwhiteFilter;
