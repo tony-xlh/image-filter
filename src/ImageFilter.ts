a9a6d9aa71bfc10deeb9ca6f3e341a1935bc5462
xulihang <xulihanghai@163.com>
2024-03-04 14:26:43 +0800

    add index.html

--- src/ImageFilter.ts
+++
@@ -1,5 +0,0 @@
-export interface ImageFilter {
-  cvs:HTMLCanvasElement;
-  process(img:HTMLImageElement|HTMLCanvasElement|HTMLVideoElement): void;
-  convert(r:number,g:number,b:number,a:number): {r:number,g:number,b:number,a:number};
-}
