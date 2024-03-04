009bd8e3b0e34bc74ccea15057cb246bfc223858
xulihang <xulihanghai@163.com>
2024-03-04 13:42:09 +0800

    package as a library

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { BlackwhiteFilter } from "./BlackwhiteFilter";
+export { ImageFilterHandler } from "./FilterHandler";
+export { GrayscaleFilter } from "./GrayscaleFilter";
+export { InvertFilter } from "./InvertFilter";
+export { SepiaFilter } from "./SepiaFilter";
+export { GenericImageFilter } from "./GenericImageFilter";
+export type { ImageFilter } from "./ImageFilter";
+
