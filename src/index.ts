ce0d167d31b7bdec7f8dd7817417e4bdff0ab829
xulihang <xulihanghai@163.com>
2025-03-05 11:23:13 +0800

    fix: export gaussian blur

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { ImageFilterHandler } from "./FilterHandler";
 export { GrayscaleFilter } from "./GrayscaleFilter";
 export { InvertFilter } from "./InvertFilter";
 export { SepiaFilter } from "./SepiaFilter";
+export { GaussianBlurFilter } from "./GaussianBlurFilter";
 export { GenericImageFilter } from "./GenericImageFilter";
 export type { ImageFilter } from "./ImageFilter";
 
